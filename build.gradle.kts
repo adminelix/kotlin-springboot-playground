9aebc55b6279d14aa9c00343a8373acca49a26bc
felix <f.bluethner@mailbox.org>
2024-10-23 08:11:58 +0200

    add missing spring-boot-starter-web dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.postgresql:postgresql")
